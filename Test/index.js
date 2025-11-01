74f0fbfb59b1c9c9d5424f0ebb0330af93e40ef1
ELOI-CODE1 <izabayoeloi2@gmail.com>
2025-11-01 16:04:20 +0200

    Deleting property

--- Test/index.js
+++ Test/index.js
@@ -3,5 +3,6 @@ car.name="Benz";
 car.plaque="RAB 437";
 car.id=20;
 car.color="Blue";
+delete car.id;
 let mine="This "+car.color+" "+car.name+" have palaque "+car.plaque+" and it is number "+car.id;
 document.getElementById('demo').innerHTML=mine;
