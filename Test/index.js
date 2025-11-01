33795fb771584de4bccdefa23e450564ea4da736
ELOI-CODE1 <izabayoeloi2@gmail.com>
2025-11-01 16:03:02 +0200

    Initial update

--- Test/index.js
+++ Test/index.js
@@ -2,4 +2,6 @@ const car={}
 car.name="Benz";
 car.plaque="RAB 437";
 car.id=20;
-document.getElementById('demo').innerHTML=car;
+car.color="Blue";
+let mine="This "+car.color+" "+car.name+" have palaque "+car.plaque+" and it is number "+car.id;
+document.getElementById('demo').innerHTML=mine;
